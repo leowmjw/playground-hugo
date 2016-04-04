b45cb36b8e7ce62e8dcf3d1bcc121624cd9a9c18
Michael Leow <leow@sinarproject.org>
2016-04-05 02:01:57 +0800

    Summary: First cut PoC done; to run full data set
    Changesets:
    - Generated some life data based on final items
    - Add the ability to breakdown adn rearrange for loading into arangodb
    - Fixed and added truncate capabilities
    - TODO: remove PROCESS/.. folder from git checks?

---
+++ TOOLS/arangodb-utility.bash
@@ -0,0 +1,7 @@
+# To be included/ sourced :P
+
+truncate_collection ()
+{
+    category=$1
+    echo "Truncating the collection ${category} in the DB sinar!"
+}
--- TOOLS/refresh-arangodb-with-popit.bash
+++ TOOLS/refresh-arangodb-with-popit.bash
@@ -5,27 +5,35 @@ echo "Sinar Project Refesh ArangoDB with PopIt Data!!"
 
 mycat=$1
 # DEBUG:
-echo "Category is $mycat"
+# echo "Category is $mycat"
 
 if [ "${mycat}X" == "X" ]
     then
     echo "ERROR"
 elif [ "${mycat}X" == "ALLX" ]
     then
+    # Trubcate DB first .. :P
+    arangosh --javascript.execute ./TOOLS/truncate-arangos.js
     # ALL
     for curcat in `ls PROCESS`
         do
+        # Source all the dependencies function for each type
+        . ./TOOLS/arangodb-utility.bash
+        
         # DEBUG:
         # echo "Going into folder PROCESS/${curcat}"
         # cd into the correct one inside PROCESS folder
         cd ./PROCESS/${curcat}
         # DEBUG:
-        echo "Got in .. now in `pwd`"
+        # echo "Got in .. now in `pwd`"        
         
-        # Source all the dependencies function for each type
         # Can use below if Bash 4 and above ..
         # curcat={$curcat,,}
         curcat=`echo ${curcat} | tr '[:upper:]' '[:lower:]'`
+        
+        # Truncate the Collection first ..
+        # Below not really needed .. :P
+        truncate_collection ${curcat} 
         
         # Use httpie to get the top level file of current category
         # DEBUG:
@@ -33,22 +41,28 @@ elif [ "${mycat}X" == "ALLX" ]
         # http http://api.popit.sinarproject.org/en/${curcat}?format=json -o ${curcat}-sample.json
         # Use jq to figure out the number ofpages to be loadded
         pages=`cat ${curcat}-sample.json | jq '.num_pages'`
+        # DEBUG:
+        # pages=1
         
         # DEBUG:
-        echo "Number of pages is ${pages}"
+        # echo "Number of pages is ${pages}"
         # From page 1 till <page_num>
-        for (( i=2; i<=${pages} ; i++ ))
+        for (( i=1; i<=${pages} ; i++ ))
             do
             # Use httpie to download the raw file; saved with <resource-name>-pg-<current_page>.json
             # DEBUG:
-            echo "Getting page ${i}"
+            echo "Getting page ${i}  =====>>>>>>>>>"
             # DEBUG:
             echo "Downloading http://api.popit.sinarproject.org/en/${curcat}?format=json&page=${i}"
+            http http://api.popit.sinarproject.org/en/${curcat}?format=json -o ${curcat}-page-${i}.json
+            
             # Extract out only results; move the key (ID) to be primary
-        
             # Save as for use by arangodbimp next; with the pattern arangodb-<resource-name>-pg-<current_page>.json
-        
+            cat ${curcat}-page-${i}.json | jq '.results[] | { _key: .id, data: values } | del(.data.id)' \
+                | jq -c '.' >arangodb-${curcat}-page-${i}.json
+                        
             # Load using arangoimp (maybe just later in one fell swoop??)
+            arangoimp --server.database=sinar --collection=${curcat} --file=arangodb-${curcat}-page-${i}.json
         done
         
         # Finish; use cd - to get back to top level directory
